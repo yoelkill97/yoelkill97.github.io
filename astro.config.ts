1698bcea517d4b2498fc25bebbd327f5af660615
yoelkill <125317997+yoelkill97@users.noreply.github.com>
2024-12-11 19:20:48 -0500

    add deploy and updating astroconfig

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
-    : 'http://localhost:4321',
+  site: process.env.CI // Dynamically set the site URL based on the environment
+    ? 'https://yoelkill97.github.io' // GitHub Pages production URL
+    : 'http://localhost:4321', // Local development URL
   integrations: [
     react(),
     tailwind({
-      applyBaseStyles: false,
+      applyBaseStyles: false, // Disable Tailwind's base styles
     }),
   ],
-})
+});
