4ee7ea3ab795dd3dcd78d1670078d56e8bc02e6a
davidhckh <david.heckhoff@gmail.com>
2025-10-13 16:33:42 +0200

    feat: legal german translations

--- src/prerender.ts
+++ src/prerender.ts
@@ -1,11 +1,9 @@
 export async function prerender(data: any) {
-  console.log("DATA", data);
-
   return {
     html: "",
     links: ["/de/test", "/test"],
     head: {
-      lang: data.url.includes("/de") ? "de" : "en",
+      lang: data.url.startsWith("/de") ? "de" : "en",
     },
   };
 }
