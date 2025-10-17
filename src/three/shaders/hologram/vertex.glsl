be41ca0ff0c007b04f57852c2365bd11655e1aee
davidhckh <david.heckhoff@gmail.com>
2025-10-17 19:53:25 +0200

    style: cleanup

--- src/three/shaders/hologram/vertex.glsl
+++ src/three/shaders/hologram/vertex.glsl
@@ -5,8 +5,7 @@ varying vec3 vPosition;
 
 uniform float uTime;
 
-float random2D(vec2 value)
-{
+float random2D(vec2 value) {
     return fract(sin(dot(value.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
 
@@ -14,16 +13,20 @@ void main() {
     #include <skinbase_vertex>
     #include <begin_vertex>
     #include <skinning_vertex>
+    
+    // --- Glitch effect ---
+    float glitchTime = uTime - transformed.y;
+    float glitchStrength = sin(glitchTime) + sin(glitchTime * 3.45) + sin(glitchTime * 8.76);
+    glitchStrength /= 2.0;
+    glitchStrength = smoothstep(0.3, 1.0, glitchStrength); // threshold effect
+    glitchStrength *= 0.02; // intensity
+    transformed.x += (random2D(transformed.xz + uTime) - 0.5) * glitchStrength;
+    transformed.z += (random2D(transformed.zx + uTime) - 0.5) * glitchStrength;
+    // ---------------------
+
     #include <project_vertex>
 
-    //float glitchTime = uTime - transformed.y;
-    //float glitchStrength = sin(glitchTime) + sin(glitchTime * 3.5) +  sin(glitchTime * 8.76);
-    //glitchStrength /= 3.0;
-    //glitchStrength = smoothstep(0.3, 1.0, glitchStrength);
-    //glitchStrength *= 0.1;
-    //transformed.x += (random2D(transformed.xz + uTime) - 0.5) * glitchStrength;
-    //transformed.z += (random2D(transformed.zx + uTime) - 0.5) * glitchStrength;
-
+    // Normal skinning
     vec4 skinnedNormal = vec4(0.0);
     skinnedNormal += boneMatX * vec4(normal, 0.0) * skinWeight.x;
     skinnedNormal += boneMatY * vec4(normal, 0.0) * skinWeight.y;
