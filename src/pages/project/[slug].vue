c09ee08088c013d4ce3ba8841fdf1762f2ed1be1
davidhckh <david.heckhoff@gmail.com>
2025-10-24 10:28:51 +0200

    style: cleanup

--- src/pages/project/[slug].vue
+++ src/pages/project/[slug].vue
@@ -1,3 +1,5 @@
+<script setup lang="ts"></script>
+
 <template>
   <h2>project</h2>
 </template>
--- src/three/shaders/includes/about-ambient.glsl
+++ src/three/shaders/includes/about-ambient.glsl
@@ -1,4 +1,4 @@
-#define AMBIENT_COLOR vec3(0.,0.039,0.129)
+#define AMBIENT_COLOR vec3(0.,0.027,0.099)
 
 vec3 applyAmbient(vec3 color) {
     return color + AMBIENT_COLOR * uProgress;
