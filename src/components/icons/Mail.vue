ffd498c335b543368951bde9b3c84c8f8f7dadc4
davidhckh <david.heckhoff@gmail.com>
2026-05-16 19:52:23 +0200

    fix: Mail icon fill

--- src/components/icons/Mail.vue
+++ src/components/icons/Mail.vue
@@ -9,6 +9,7 @@
       stroke="currentColor"
       stroke-width="var(--stroke-md)"
       vector-effect="non-scaling-stroke"
+      fill="none"
     />
     <path
       d="M16.5 46.5L114.918 126.742C122.25 132.72 132.768 132.743 140.126 126.797L239.5 46.5"
@@ -17,6 +18,7 @@
       vector-effect="non-scaling-stroke"
       stroke-linecap="round"
       stroke-linejoin="round"
+      fill="none"
     />
   </svg>
 </template>
